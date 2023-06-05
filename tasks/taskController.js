7820ba0391346d6bbfd7783be05d993b242421db
willotavio <willaotavio@gmail.com>
2023-06-05 19:39:43 -0300

    Add delete task button

--- tasks/taskController.js
+++ tasks/taskController.js
@@ -7,7 +7,6 @@ router.get('/tasks', (req, res) => {
     Task.findAll({
         raw: true
     }).then((tasks) => {
-        console.log(tasks);
         res.render('tasks/tasksHome', {
             tasks: tasks
         });
@@ -26,6 +25,17 @@ router.post('/tasks/save', (req, res) => {
         title: title,
         description: description,
         deadline: deadline
+    }).then(() => {
+        res.redirect('/tasks');
+    });
+});
+
+router.post('/tasks/delete', (req, res) => {
+    const id = req.body.taskId;
+    Task.destroy({
+        where: {
+            id: id
+        }
     }).then(() => {
         res.redirect('/tasks');
     });

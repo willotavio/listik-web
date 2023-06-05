2ea8d98eb5d0765a9e703035e797a2dc741ec709
willotavio <willaotavio@gmail.com>
2023-06-05 16:42:59 -0300

    Add tasks routes

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.set('view engine', 'ejs');
-app.use(express.static('./public'));
+app.use(express.static('public/'));
 app.use(express.urlencoded({
     extended: true
 }));
@@ -17,13 +17,12 @@ connection.authenticate()
         console.log(err);
     });
 
-const taskModel = require('./tasks/taskModel');
-const taskController = require('./tasks/taskController');
-app.use('/', taskController);
-
 app.get('/', (req, res) => {
     res.render('home');
 });
+
+const tasksController = require('./tasks/taskController');
+app.use('/', tasksController);
 
 app.listen(3000, () => {
     console.log("Server On!");
--- tasks/taskController.js
+++ tasks/taskController.js
@@ -1,8 +1,31 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/new-task', (req, res) => {
-    res.render('tasks/addTask');
+const Task = require('./taskModel');
+
+router.get('/tasks', (req, res) => {
+    res.render('tasks/tasksHome');
+});
+
+router.get('/tasks/new', (req, res) => {
+    Task.findAll({
+        raw: true
+    }).then((result) => {
+        res.render('tasks/addTask', result);
+    });
+});
+
+router.post('/tasks/save', (req, res) => {
+    const title = req.body.taskTitle;
+    const description = req.body.taskDescription;
+    const deadline = req.body.taskDeadline;
+    Task.create({
+        title: title,
+        description: description,
+        deadline: deadline
+    }).then(() => {
+        res.redirect('tasks/');
+    });
 });
 
 module.exports = router;

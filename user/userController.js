7f27450b05dbe276688e831adcd05815b98907f2
willotavio <willaotavio@gmail.com>
2023-06-08 19:55:10 -0300

    Add login and signup system

--- user/userController.js
+++ user/userController.js
@@ -1,12 +1,65 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+
+const User = require('./userModel');
 
 router.get('/login', (req, res) => {
     res.render('auth/login');
 });
 
+router.post('/login', (req, res) => {
+    const userEmail = req.body.userEmail;
+    const userPassword = req.body.userPassword;
+
+    User.findOne({where: {
+        userEmail: userEmail,
+    }}).then((result) => {
+        if(result){
+            bcrypt.compare(userPassword, result.userPassword).then((match) => {
+                if(match){
+                    res.redirect('/tasks');
+                }
+                else{
+                    res.redirect('/login');
+                }
+            });
+        }
+        else{
+            res.redirect('/login');
+        }
+    });
+});
+
 router.get('/signup', (req, res) => {
     res.render('auth/signup');
+});
+
+router.post('/signup', (req, res) => {
+    const userName = req.body.userName;
+    const userEmail = req.body.userEmail;
+    const userPassword = req.body.userPassword;
+
+    User.findOne({where: {
+        userEmail: userEmail
+    }}).then((result) => {
+        if(!result){
+            const salt = 10;
+            bcrypt.hash(userPassword, salt).then((password) => {
+                User.create({
+                    userName: userName,
+                    userEmail: userEmail,
+                    userPassword: password
+                }).then(() => {
+                    res.redirect('/tasks');
+                });    
+            });
+        }
+        else{
+            res.redirect('/signup');
+        }
+    })
+
 });
 
 module.exports = router;
